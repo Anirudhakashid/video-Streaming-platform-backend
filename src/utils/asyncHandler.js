068cf5891189703071d46b2667ec1b7379548032
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-07-14 19:47:24 +0530

    add asyncHandler utility for handling async operations in Express routes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+// function to handle async operations in Express routes
+// helps to avoid try-catch blocks in every route handler
+// Errors are automatically passed to Express's error middleware via next(err).
+
+const asyncHandler = (requestHandler) => (req, res, next) =>
+  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+
+export { asyncHandler };
