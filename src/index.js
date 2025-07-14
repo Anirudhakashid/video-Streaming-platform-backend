8190dcd3d7e9d262f2ab5f65d2654016004c6e5f
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-07-14 19:47:11 +0530

    code clean up

--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
-//* configure dotenv to make the variables available to all the files as soom as possible
+//* configure dotenv to make the variables available to all the files as soon as possible
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-//* 2nd approach - connection code in db folder
-connectDB();
-
-//** */ 1st Approach - it makes index.js too crowded
-/*
-import express from "express";
-const app = express();
-
-//* Connect to MongoDB via IIFE
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-
-    //* this line listens for the error event emitted by the Express app object
-    app.on("error", (error) => {
-      console.error("Connection error:", error);
-      throw error;
-    });
-
-    //* listen after successfull connection
-    app.listen(process.env.PORT, () => {
-      console.log(`App listing on port: ${process.env.PORT} `);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
+connectDB()
+  .then(() => {
+    app
+      .on("errors", (error) => {
+        console.log("Error: ", error);
+        throw error;
+      })
+      .listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+      });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Failed: ", err);
+  });
