ae2a32976a8fc1d381c4a3b72a1d7856d40ca7ee
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-07-17 13:29:57 +0530

    route imports and middleware setup in app.js

--- src/app.js
+++ src/app.js
@@ -11,17 +11,19 @@ app.use(
     credentials: true,
   })
 );
-
 //* configuration to accept data in form of json with a limit
 app.use(express.json({ limit: "16kb" }));
-
 //* configuration to accept data in form of url-encoded
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 //* configuration to static/publically available files
 app.use(express.static("public"));
-
 //* configuration to parse cookies-to perform curd operations on user cookies
 app.use(cookieParser());
+
+//Routes:
+import userRouter from "./routes/user.routes.js";
+
+//Router declaration: https//localhost:8000/api/v1/users = on hiting this the control will be passed to userRouter
+app.use("/api/v1/users", userRouter);
 
 export { app };
