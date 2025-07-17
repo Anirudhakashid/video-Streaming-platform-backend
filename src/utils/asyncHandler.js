dd11d8631ac82b74aa4c53aef26f72c335a872f3
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-07-17 13:29:29 +0530

    fix asyncHandler typo and structure

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,10 @@
 // helps to avoid try-catch blocks in every route handler
 // Errors are automatically passed to Express's error middleware via next(err).
 
-const asyncHandler = (requestHandler) => (req, res, next) =>
-  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 export { asyncHandler };
