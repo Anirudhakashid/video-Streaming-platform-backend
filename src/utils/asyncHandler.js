4f00e6d3e62faa5da0392067586479ca9b00e084
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-07-18 14:04:06 +0530

    add clarification comment

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,7 @@
 // function to handle async operations in Express routes
 // helps to avoid try-catch blocks in every route handler
 // Errors are automatically passed to Express's error middleware via next(err).
+//In Express.js, if an async function throws an error or a promise rejects, the error won’t automatically get passed to the next() function, and your app might crash or hang silently.
 
 const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
