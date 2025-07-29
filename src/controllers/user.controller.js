289176d9d37668dc7b2f0aa74cb6f0c0d9cb7ff7
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-07-29 16:13:11 +0530

    feat: implement user login and logout functionality with token management

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,18 +4,34 @@ import { User } from "../models/user.model.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-// Contains the logic for what happens when someone tries to register:
-// get user details from frontend
-// validation-[not empty]
-// check if user already exits - using email and username
-// check for images and avatar
-// if present upload them to cloudinary, avatar specially
-// create user object - create entry in DB
-// remove password and refresh token from the response
-// check for used creation
-// return response
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    //store refresh token in db
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "error generating access and refresh tokens");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
+  //TODO: registerUser
+  // get user details from frontend
+  // validation-[not empty]
+  // check if user already exits - using email and username
+  // check for images and avatar
+  // if present upload them to cloudinary, avatar specially
+  // create user object - create entry in DB
+  // remove password and refresh token from the response
+  // check for used creation
+  // return response
+
   const { userName, email, fullName, password } = req.body;
   // console.log("Email: ", email);
 
@@ -90,4 +106,90 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User Registered Successfully!"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  //TODO: loginUser
+  //req.body -> data
+  //check username and email exists
+  //find the user
+  //check password
+  //generate access and refresh tokens
+  //send cookies(access and refresh token)
+
+  const { email, userName, password } = req.body;
+
+  if (!userName || !email) {
+    throw new ApiError(400, "userName or email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [email, userName],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "user does not exists");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  //sending cookies
+  //cookies can be modified by frontend/anyone hence this is the security step to allow only server to modify cookie
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User loggedIn successfully!"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  //got the user info from the custom verifyJWT middleware
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined },
+    },
+    {
+      new: true,
+    }
+  );
+
+  //update the cookie
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie(accessToken, options)
+    .clearCookie(refreshToken, options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+export { registerUser, loginUser, logoutUser };
