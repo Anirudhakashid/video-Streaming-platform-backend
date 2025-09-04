013902934aa06e0252025e61fe0893b25a8d6f13
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-09-04 17:24:10 +0530

    docs: update comments for clarity in subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 
+// a document contains a subscriber and a channel
+// to keep track of who is subscribed to whom
+// to count a user's (channels) subscribers we count the number of documents where that channel is present
+// to get a user's (subscriber) subscriptions we find all documents where that user is the subscriber
+
 const subscriptionScheme = new Schema(
   {
     subscriber: {
