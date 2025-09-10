5a10772a8b1500f4f3194adcb6eca3ee2eb0f303
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-09-10 18:16:57 +0530

    feat: reorganize comment routes to include add, update, and delete functionalities

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
-import { getVideoComments } from "../controllers/comment.controller";
+import {
+  addComment,
+  deleteComment,
+  getVideoComments,
+  updateComment,
+} from "../controllers/comment.controller";
 
 const router = Router();
 
 // Middleware to verify JWT for all routes in this router
 router.use(verifyJWT);
+
+router.route("/getVideoComments/:videoId").get(getVideoComments);
+router.route("/addComment/:videoId").post(addComment);
+router.route("/updateComment/:commentId").put(updateComment);
+router.route("/deleteComment/:commentId").delete(deleteComment);
 
 export default router;
