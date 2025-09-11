9ca58ca3feefc873cf52833da7502894c833e729
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-09-11 18:49:08 +0530

    feat: implement toggleVideoLike function for liking and unliking videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,39 @@ import mongoose from "mongoose";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 
+//toggle like for a video
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const userId = req.user?._id;
+
+  if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+    throw new ApiError(400, "Unauthorized access");
+  }
+
+  if (!videoId || !mongoose.Types.ObjectId.isValid(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  //check if like already exists
+  const existingLike = await Like.findOne({ video: videoId, likedBy: userId });
+  if (existingLike) {
+    // If like exists, remove it
+    await Like.deleteOne({ _id: existingLike._id });
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "Video unliked successfully"));
+  } else {
+    // If like doesn't exist, create a new like
+    const newLike = await Like.create({ video: videoId, likedBy: userId });
+    if (!newLike) {
+      throw new ApiError(500, "Failed to like the video");
+    }
+    return res
+      .status(201)
+      .json(new ApiResponse(201, newLike, "Video liked successfully"));
+  }
+});
+
 //get all liked videos for a user
 const getLikedVideos = asyncHandler(async (req, res) => {
   const userId = req.user?._id;
@@ -49,4 +82,4 @@ const getLikedVideos = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, videos, "Liked videos fetched successfully"));
 });
 
-export { getLikedVideos };
+export { getLikedVideos, toggleVideoLike };
