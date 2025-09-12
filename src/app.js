2f81945c76b1607caa46370e87cbde600a7b0093
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-09-12 11:51:47 +0530

    feat: add LikeRouter for handling like-related routes

--- src/app.js
+++ src/app.js
@@ -23,9 +23,11 @@ app.use(cookieParser());
 //Routes:
 import userRouter from "./routes/user.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import LikeRouter from "./routes/like.routes.js";
 
 //Router declaration: https//localhost:8000/api/v1/users = on hiting this the control will be passed to userRouter
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", LikeRouter);
 
 export { app };
